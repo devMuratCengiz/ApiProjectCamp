2f2d5e183058974e4798aaeffdaec7fc5815a0f8
Murat Cengiz <devmuratcengiz@outlook.com>
2025-02-07 11:07:36 +0300

    Add project files.

---
+++ ApiProjectCamp.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjectCamp.WebApi.Entities
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
