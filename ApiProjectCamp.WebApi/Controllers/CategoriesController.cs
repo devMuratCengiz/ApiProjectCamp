50f0fecbfa4d354fa6766012044d58b2fcbcadb3
Murat Cengiz <devmuratcengiz@outlook.com>
2025-02-10 20:16:56 +0300

    Chef and Category Controller Added

---
+++ ApiProjectCamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,64 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+
+    }
+}
---
+++ ApiProjectCamp.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,55 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var chefs = _context.Chefs.ToList();
+            return Ok(chefs);
+        }
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var chef = _context.Chefs.Find(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+            _context.Chefs.Remove(chef);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetChefById(int id)
+        {
+            var chef = _context.Chefs.Find(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+            return Ok(chef);
+        }
+    }
+}
